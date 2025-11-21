9b49b4b22b9baab68972827b2e8a40e074b1bf3f
Corey Barbera <160194877+Dismodified@users.noreply.github.com>
2025-11-21 17:32:38 -0500

    Update locks.h

--- locks.h
+++ locks.h
@@ -2,6 +2,7 @@
 #define LOCKS_H
 
 #include <semaphore.h>
+#include <sys/time.h>
 long long current_timestamp(void);
 
 // ========== RW Lock Implementation (from OSTEP) ==========
@@ -12,19 +13,17 @@ typedef struct _rwlock_t {
 } rwlock_t;
 
 void log_message(int priority, const char *message);
-
 void rwlock_init(rwlock_t *rw);
-
 void rwlock_acquire_readlock(rwlock_t *rw, int priority);
-
 void rwlock_release_readlock(rwlock_t *rw, int priority);
-
 void rwlock_acquire_writelock(rwlock_t *rw, int priority);
-
 void rwlock_release_writelock(rwlock_t *rw, int priority);
-
 void rwlock_destroy(rwlock_t *rw);
+void increment_lock_acquisition(void);
+void increment_lock_release(void);
+
+int get_total_acquisitions(void);
+int get_total_releases(void);
 
 
 #endif
-
